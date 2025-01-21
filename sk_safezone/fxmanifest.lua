1bf12e97a485da8e714e2c460767280c294bcf52
Snooppikoira <133780594+Snooppikoira@users.noreply.github.com>
2025-01-21 21:45:28 +0200

    Update fxmanifest.lua

--- sk_safezone/fxmanifest.lua
+++ sk_safezone/fxmanifest.lua
@@ -5,7 +5,6 @@ lua54 'yes'
 
 shared_scripts {'config.lua', '@ox_lib/init.lua', '@es_extended/imports.lua'}
 client_scripts {'client/*.lua'}
-server_scripts {'server/*.lua'}
 
 files {'web/index.html'}
 ui_page 'web/index.html'
